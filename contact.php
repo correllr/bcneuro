202529996d4af58a6fefe8514f4953d28e02d256
Robbie <correllr@gmail.com>
2018-07-16 12:17:25 +0200

    H1 aangespast

--- contact.php
+++ contact.php
@@ -17,7 +17,9 @@
 			</div>
 		</div>
 		
-		<iframe id="contact-maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3126.9502740904177!2d-0.524350985162887!3d38.39639878392488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6236ab98282547%3A0x67a1bba112c89381!2sCalle+Cervantes%2C+25%2C+03690+San+Vicente+del+Raspeig%2C+Alicante!5e0!3m2!1snl!2ses!4v1489409154813" width="100%" height="420" frameborder="0" style="border:0" allowfullscreen></iframe>
+	<!-----	
+	<iframe id="contact-maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3126.9502740904177!2d-0.524350985162887!3d38.39639878392488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6236ab98282547%3A0x67a1bba112c89381!2sCalle+Cervantes%2C+25%2C+03690+San+Vicente+del+Raspeig%2C+Alicante!5e0!3m2!1snl!2ses!4v1489409154813" width="100%" height="420" frameborder="0" style="border:0" allowfullscreen></iframe>
+	--->
 
 
 	</div>
