79d2d4720c89564d6b86df0cbf0bc13e0840fef9
Robbie <correllr@gmail.com>
2018-07-18 16:03:13 +0200

    Fixed

---
+++ contact.php
@@ -0,0 +1,31 @@
+<div id="contact">
+	<i id="contact-close" class="fas fa-window-close"></i>
+	<div id="contact-container">
+		<h1 id="contact-title">
+		Contact
+		</h1>
+		
+		<div id="contact-flex">
+			<h3 id="contact-information">
+			To book a course or for any questions, you can reach me on:<br>
+<<<<<<< HEAD
+			<a href="tel:00447494984332">+447494 984332</a><br>
+			<a href="mailto:jake@bcneuro.co.uk" target="_top">jake@bcneuro.co.uk</a><br>
+=======
+			<a href="tel:00447958197979">+447958 197979</a><br>
+			<a href="mailto:someone@example.com" target="_top">jake@bcneuro.co.uk</a><br>
+>>>>>>> ddd2a8e6c6dd39482fd0d2ca4a95cc0e43a4f286
+			<a id="contactfblink" href="https://www.facebook.com/bcneuro/"><i class="fab fa-facebook-square fbicon"></i></a>
+			</h3>
+			<div id="contact-form">
+			 <?php echo do_shortcode('[contact-form-7 id="49" title="Untitled"]'); ?>
+			</div>
+		</div>
+
+
+	</div>
+</div>
+
+<div id="contact-open" class="open-contact">
+	Contact
+</div>
